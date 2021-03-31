3a87c5f6eea644b8820002ad3379d961a08c7146
Shahim Shaar <shahmshaar5@gmail.com>
2021-03-31 03:16:12 -0500

    woww

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ set -e
 npm run build
 # navigate into the build output directory
 cd dist
-git init
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:shahimshaar/shahimshaar.github.io.git main
