735484ac4a0e47bd9b544c9b6293821f130deef0
Shahim Shaar <shahmshaar5@gmail.com>
2021-03-31 02:29:42 -0500

    my brain cells in pain6

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div class="container">
-  <h1>let's go</h1>
   <resume />
-  </div>
 </template>
 
 <script>
@@ -11,18 +8,7 @@ import resume from './resume.vue'
 export default {
   name: 'App',
   components: {
-    resume
-  }
+    resume,
+  },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
