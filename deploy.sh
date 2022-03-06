d40daa9e7c97679c5811a171fc0f3e32eb9671b8
shahimshaar <shahmshaar5@gmail.com>
2022-03-05 18:33:02 -0600

    Clean Slate - deleted all contents

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-set -e
-# build
-npm run build
-# navigate into the build output directory
-cd dist
-git add -A
-git commit -m 'deploy'
-git push -f git@github.com:shahimshaar/shahimshaar.github.io.git main
-cd -
